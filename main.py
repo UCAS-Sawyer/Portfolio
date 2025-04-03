b24d52b0bd1fde5008b81d040e294cce7dad539e
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-04-03 14:54:37 -0600

    added the descriptions

--- main.py
+++ main.py
@@ -20,23 +20,34 @@ def main():
 
     # Checking input
     if choice == '1':
-        time.sleep(1)
+        print("\nThis project solves the Coin Change Problem. It was very fun to do and I learned a lot. I learned more on how to use csvs.")
+        time.sleep(4)
         coin_change_main()
+
     elif choice == '2':
-        time.sleep(1)
+        print("\nThis project allows you to create characters, fight with characters and level them up, and see theirs stats with the use of different charts. I liked this project, even though it was hard a times. I learned a lot on how to use some python libraries.")
+        time.sleep(4)
         battle_simulator_main()
+
     elif choice == '3':
-        time.sleep(1)
+        print("\nThis project was one of my finals. It is a little adventuer/dungeon crawler game. It was very fun to do and it really was a test of my skills. It helped me to develope my general probelm solving and programing skills.")
+        time.sleep(4)
         shortest_dungeon_crawler_main()
+
     elif choice == '4':
-        time.sleep(1)
+        print("\nThis project creates a password with some conditions it is given. This one I made earlier in my programming life, so I didn't know much. I learned lots on conditionals and the like.")
+        time.sleep(4)
         random_password_generator_main()
+
     elif choice == '5':
-        time.sleep(1)
+        print("\nThis project counts the number of words in a txt file that you give it. This one was a challenge for me, because I was just learning how to use fiels. It expounded my knowledge of using files.")
+        time.sleep(4)
         number_lines = number_of_lines()
         to_do_list_main(number_lines)
+
     elif choice == '6':
-        time.sleep(1)
+        print("\nThis project uses a ceaser cipher on anyword you give it. It can encode a word or decode it if it used the same ceaser cipher. It was very fun to do and I learned a lot about lists and for loops.")
+        time.sleep(4)
         cipherifyer9000_main()
     elif choice == '7':
         print("\nExiting the Portfolio. Goodbye!")
