18fa1baca7aa2eae800aa2078250aeb06e6779a0
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-04-03 14:41:17 -0600

    added cipher

--- Cipherifyer9000_main.py
+++ Cipherifyer9000_main.py
@@ -1,37 +1,43 @@
 #Sawyer Wood, ProficiencyTest Secret Sypher.
-word = input("What is the word you want to Cipher/Decipher?: ")
-idk = input("Do you want to cipherify or decipherify?: ")
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1"]
-shiftedalphabet = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", ":", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "1"]
+def main():
+    idk = input("\nDo you want to 1. Cipher, 2. Decipher?: ")
+    word = input("\nWhat is the word you want to cipher/decipher?: ")
 
-def Cipherify(word):
-    wordspot = 0
-    alphabetspot = 0
-    stopoint = len(word)
-    while wordspot < stopoint:
-        if word[wordspot] == alphabet[alphabetspot]:
-            letter = shiftedalphabet[alphabetspot]
-            wordspot = wordspot + 1
-            alphabetspot = 0
-            print(letter)
-        else:
-            alphabetspot = alphabetspot + 1
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1"]
+    shiftedalphabet = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", ":", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "1"]
 
-def Decipherify(word):
-    wordspot = 0
-    alphabetspot = 0
-    stopoint = len(word)
+    def Cipherify(word):
+        cipheredword = ""
+        wordspot = 0
+        alphabetspot = 0
+        stopoint = len(word)
+        while wordspot < stopoint:
+            if word[wordspot] == alphabet[alphabetspot]:
+                letter = shiftedalphabet[alphabetspot]
+                wordspot = wordspot + 1
+                alphabetspot = 0
+                cipheredword = cipheredword + letter
+            else:
+                alphabetspot = alphabetspot + 1
+        print(f"\n{cipheredword}")
 
-    while wordspot < stopoint:
-        if word[wordspot] == shiftedalphabet[alphabetspot]:
-            letter = alphabet[alphabetspot]
-            wordspot = wordspot + 1
-            alphabetspot = 0
-            print( letter)
-        else:
-            alphabetspot = alphabetspot + 1
+    def Decipherify(word):
+        decipheredword = ""
+        wordspot = 0
+        alphabetspot = 0
+        stopoint = len(word)
 
-if idk == str("cipher"):
-    Cipherify(word)
-elif idk == str("decipher"):
-    Decipherify(word)
+        while wordspot < stopoint:
+            if word[wordspot] == shiftedalphabet[alphabetspot]:
+                letter = alphabet[alphabetspot]
+                wordspot = wordspot + 1
+                alphabetspot = 0
+                decipheredword = decipheredword + letter
+            else:
+                alphabetspot = alphabetspot + 1
+        print(f"\n{decipheredword}")
+
+    if idk == str("1"):
+        Cipherify(word)
+    elif idk == str("2"):
+        Decipherify(word)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import time
 
 # Importing main functions from different modules
 from Coin_change_problem_main import main as coin_change_main
@@ -10,25 +11,34 @@ from ShortestDoungeonCrawlerEvermain import main as shortest_dungeon_crawler_mai
 from RandomPasswordGenratormain import main as random_password_generator_main
 from ToDoList_main import main as to_do_list_main
 from ToDoList_main import number_of_lines as number_of_lines
+from Cipherifyer9000_main import main as cipherifyer9000_main
 
 def main():
     # Display the menu options
-    print("\nPlease choose an option:\n1. Coin Change Problem\n2. Battle Simulator\n3. Shortest Dungeon Crawler Ever\n4. Random Password Generator\n5. To-Do List\n6. Exit")
-    choice = input("Enter your choice (1-6): ")
+    print("\nPlease choose an option:\n1. Coin Change Problem\n2. Battle Simulator\n3. Shortest Dungeon Crawler Ever\n4. Random Password Generator\n5. To-Do List\n6. Cipherifyer9000\n7. Exit")
+    choice = input("Enter your choice (1-7): ")
 
     # Checking input
     if choice == '1':
+        time.sleep(1)
         coin_change_main()
     elif choice == '2':
+        time.sleep(1)
         battle_simulator_main()
     elif choice == '3':
+        time.sleep(1)
         shortest_dungeon_crawler_main()
     elif choice == '4':
+        time.sleep(1)
         random_password_generator_main()
     elif choice == '5':
+        time.sleep(1)
         number_lines = number_of_lines()
         to_do_list_main(number_lines)
     elif choice == '6':
+        time.sleep(1)
+        cipherifyer9000_main()
+    elif choice == '7':
         print("\nExiting the Portfolio. Goodbye!")
         sys.exit()  # Exit the program
     else:
